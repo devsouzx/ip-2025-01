8109f16179d79b4988fa286cbeb9fccb31ba4829
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-03-23 15:53:39 -0300

    17 - Série de pares

---
+++ 17 - Série de pares/code.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+	if x % 2 != 0 {
+		fmt.Println("O PRIMEIRO NUMERO NAO E PAR")
+	} else {
+		for i := 0; i < y; i++ {
+			fmt.Printf("%d ", x)
+			x += 2
+		}
+	}
+}
